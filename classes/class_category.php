5065b48a6451c55601bb246b683db4db4c490186
Alfredo <21030761@itcelaya.edu.mx>
2024-04-17 14:16:51 -0600

    Dsiplay table  demo

--- classes/class_category.php
+++ classes/class_category.php
@@ -36,37 +36,24 @@
         }
 
         function displayData($query){
-            $html = '<div class="margin-auto">';
-            $datos='<table class="margin-y-auto overflow-x-auto border-radius-20 padding-10">';
+            $htmlStart = '<div class="margin-auto width-100 padding-20">';
+            $datos='<table class="margin-auto margin-y-auto overflow-x-auto border-radius-20 padding-10">';
             $this->query($query);
             $this->getRecord($query);
 
-            $datos.='<div class="TITLE flex"><h3>Categoría</h3> <button><i class="fa-solid fa-plus"></i></button></div>';
+            $datos.='<div class="flex justify-center"><h3>Categoría</h3> <button><i class="fa-solid fa-plus"></i></button></div>';
             
-            $datos.='<thead><tr>';
             // Fila de encabezados
+            $datos.='<thead><tr>';
             $campos=array();
-            $tablaN=$this->getFields($campos);
+            // $tablaN=$this->getFields($campos);
             foreach($campos as $campo){
                 $datos.='<th>'.$campo.'</th>';
             }
             $datos.="<th>&nbsp</th><th>&nbsp</th>";
-                //$datos.='</table></div>';
-                //echo($datos);
             $datos.='</tr></thead>';
                 
-            //var_dump($tablaN);
-            //$header='<span> class="badge bg-info">'.$tablaN.'<button class="btn btn-success"><i class="fa-solid fa-plus"></i></button></span>';
-            
-            //$header='<span> class="badge bg-info">'.$datos.'<button class="btn btn-success"><i class="fa-solid fa-plus"></i></button></span>';
-            
-            // EST ESTÁ MAL
-            // foreach($this->registrersBlock as $row){
-            //     $datos.='<tr>';
-            //     $datos.='<td class="col-1"><button><i class="fa-regular fa-trash-can"></i></button></td>';
-            //     $datos.='<td class="col-1><button><i class="fa-solid fa-pen-to-square"></i></button></td>';
-            // }
-
+            // Contenido y datos
             $datos.='<tbody>';
             foreach ($this->registrersBlock as $row) {
                 $datos.='<tr>';
@@ -79,56 +66,10 @@
             }
             $datos.='</tbody>';
             $datos.='</table></div>';
-            echo($datos);
-
-            // $datos.='</table></div>';
-            // printf($html.$datos);
-            //return $html.$datos;
-            //echo ($html.$datos);
-
-            //echo ($html.$header.$datos);
-
-            // ESTE ES EL MÍO
-            // foreach($this->registrersBlock as $row){
-            //     foreach($row as $fieldRow){
-            //         $html .= $fieldRow." ";
-            //     }
-            //     $html .= "<br>";
-            // }
-            // $html .= "</div>";
-            // //return $html;
-            // echo($html);
+            $htmlEnd = '</div>';
+            echo($htmlStart.$datos.$htmlEnd);
         }
 
-        function displayData2($query){
-            $html = '<div class="margin-auto">';
-            $html .= '<table class="margin-y-auto overflow-x-auto border-radius-20 padding-10 width-100">';
-            
-            // Encabezados de la tabla
-            $html .= '<thead><tr>';
-            $campos = array();
-            $tablaN = $this->getFields($campos);
-            foreach($campos as $campo){
-                $html .= '<th>'.$campo.'</th>';
-            }
-            $html .= '</tr></thead>';
-        
-            // Datos de la tabla
-            $html .= '<tbody>';
-            foreach ($this->registrersBlock as $row) {
-                $html .= '<tr>';
-                foreach($row as $columna)
-                    $html .= '<td>'.$columna.'</td>';
-                $html .= "</tr>";
-            }
-            $html .= '</tbody>';
-            $html .= '</table></div>';
-        
-            // Mostrar el HTML
-            echo ($html);
-        }
-
-        
     }
 
     $categoryObject = new Category();
